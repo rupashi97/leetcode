d1f4f8e42852a6be4d8e637da138d6930551dda6
Rupashi <rupsangal@gmail.com>
2022-07-06 17:00:31 +0530

    leet

--- twoSum.py
+++ twoSum.py
@@ -1,4 +1,9 @@
 
+'''
+Use hash table [dictionary in python] logic as lookup complexity in hash table ~ O(1).
+Overall code complexity : O(n) with one pass
+'''
+
 class Solution:
 
     def twoSum(self, nums: list, target: int) -> list:
