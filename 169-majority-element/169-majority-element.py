5e455f7af0cab13c1be106dbd69ceda04f923a59
Rupashi Sangal <rupsangal@gmail.com>
2022-10-05 15:54:04 -0700

    Time: 482 ms (6.29%), Space: 15.7 MB (34.10%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,17 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        
+        hashTable = Counter(nums)  # O(n)
+        
+        maj  = int(len(nums)/2)
+        
+        for key,val in hashTable.items():
+            if val>maj:
+                maj=val
+                k = key
+                
+        
+        return k
+            
