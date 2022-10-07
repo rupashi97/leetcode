22afb1754022dde7ab8ae8dbd09030b67739e081
Rupashi Sangal <rupsangal@gmail.com>
2022-10-06 22:05:53 -0700

    Time: 463 ms (9.18%), Space: 15.5 MB (85.48%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -2,8 +2,16 @@ from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
+        # Boyer - Moore Voting Algorithm
+        count=0
         
-        nums.sort()
-        n = len(nums)
-        
-        return nums[int(n/2)]
+        for n in nums:
+            if count==0:
+                candidate = n
+            
+            count += (1 if n==candidate else -1)
+            
+            
+        return candidate
+            
+            
