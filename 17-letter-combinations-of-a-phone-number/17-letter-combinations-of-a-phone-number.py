70c4758e94ab7b40cb922310cb75e6e5dbd01ca3
Rupashi Sangal <rupsangal@gmail.com>
2022-10-07 08:58:23 -0700

    Time: 60 ms (29.92%), Space: 13.9 MB (79.19%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+import itertools
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        op = []
+        ip = []
+        
+        mp = {
+             2: ['a','b','c'],
+             3: ['d', 'e', 'f'],
+             4: ['g','h','i'],
+             5: ['j','k','l'],
+             6: ['m','n','o'],
+             7: ['p','q','r','s'],
+             8: ['t','u','v'],
+             9: ['w','x','y','z']
+             }
+        
+        if not digits:  # can combine
+            return []
+        
+        if len(digits)==1:
+            return mp[int(digits)]
+        
+        for i in range(len(digits)):
+            ip.append(mp[int(digits[i])])
+            
+        op = itertools.product(*ip)
+        op2 = ["".join(x) for x in op]
+        return op2
