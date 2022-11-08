321860c9768e1b17ff15ca94afa5085991a6802f
Rupashi Sangal <rupsangal@gmail.com>
2022-11-08 12:27:52 -0800

    Time: 79 ms (29.78%), Space: 14 MB (38.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,26 +4,14 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        i, j, k = 0, 0, 0
-        nums3 = [0]*(m+n)
+        i, j = m-1, n-1
         
-        while i<m and j<n:
-            if nums1[i]<=nums2[j]:
-                nums3[k] = nums1[i]
-                i+=1
+        for k in reversed(range(m+n)):
+            if j<0: break
+            if i>=0 and nums1[i]>=nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
             else:
-                nums3[k] = nums2[j]
-                j+=1
-            k+=1
-        
-        for x in range(i,m):
-            nums3[k] = nums1[x]
-            k+=1
+                nums1[k]=nums2[j]
+                j-=1
             
-        for x in range(j,n):
-            nums3[k] = nums2[x]
-            k+=1
-            
-        for i in range(len(nums3)):
-            nums1[i]=nums3[i]
-        
