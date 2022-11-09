b5f270510fa12cd18f27160fad4eaec26568470c
Rupashi Sangal <rupsangal@gmail.com>
2022-11-09 07:38:15 -0800

    Time: 102 ms (45.32%), Space: 14.1 MB (15.97%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -2,7 +2,9 @@ import collections
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        track = collections.Counter(nums1)
+        track = collections.defaultdict(int)
+        for n in nums1: track[n] += 1
+
         ans = []
         for n in nums2:
             if track[n]:
