eed356e3b58805be2313f07b98266524129f5c6f
Rupashi Sangal <rupsangal@gmail.com>
2022-11-09 09:55:35 -0800

    Time: 119 ms (32.97%), Space: 14.5 MB (67.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,17 @@
+import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
+        freqs = [0]*26
+        freqt = [0]*26
         
-        return sorted(s)==sorted(t)
+        for c in s:
+            i = ord(c) - ord('a')
+            freqs[i] += 1
+            
+        for c in t:
+            i = ord(c) - ord('a')
+            freqt[i] += 1
+        
+        return freqs == freqt
+             
