fee57a79c415d2d05ba367f8cbadd35bd6302c89
Rupashi Sangal <rupsangal@gmail.com>
2022-11-10 10:21:12 -0800

    Time: 216 ms (97.20%), Space: 25.3 MB (96.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,11 +5,9 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        if k>n: k = k%n
+        k = k%n
         res = nums[n-k:] + nums[0:n-k]
         
         for i in range(n):
             nums[i] = res[i]
-            
-            
-    
+
