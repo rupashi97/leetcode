2d2ef5547c160caa97c05cb7ba03a8e7f2a71328
Rupashi Sangal <rupsangal@gmail.com>
2022-11-10 09:23:04 -0800

    Time: 50 ms (69.88%), Space: 13.8 MB (97.56%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -7,13 +7,8 @@ class Solution:
         
         res = [[1],[1,1]]
         
-            
-        for i in range(2, numRows):
-            prevarr = res[i-1]
-            temp = []
-            for j in range(i-1):
-                temp.append(prevarr[j]+prevarr[j+1])
-                
+        for i in range(2, numRows):   
+            temp = [ res[i-1][j] + res[i-1][j+1]  for j in range(i-1)]
             res.append([1] + temp + [1])   
             
         return res 
