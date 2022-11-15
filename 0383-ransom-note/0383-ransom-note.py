ad3cfba086530e6eeeb46bd40e4a29ceda87fb06
Rupashi Sangal <rupsangal@gmail.com>
2022-11-15 13:14:51 -0800

    Time: 104 ms (57.99%), Space: 14.3 MB (19.77%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,11 +2,11 @@ import collections
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
+        if len(magazine)<len(ransomNote): return False
         track = collections.Counter(magazine)
         
         for r in ransomNote:
             if not track[r]: return False
             track[r] -=1
-            
         
         return True
